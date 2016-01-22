b97b02ee08c8615be0aef20e1894b9074a87e497
Yuri M <yuri.canada@live.com>
2016-01-22 15:59:28 -0800

    Upgraded to MahApps window

--- Upkeep.Shell/Shell.xaml.cs
+++ Upkeep.Shell/Shell.xaml.cs
@@ -12,15 +12,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace Upkeep.Shell
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Shell : MetroWindow
     {
-        public MainWindow()
+        public Shell()
         {
             InitializeComponent();
         }
