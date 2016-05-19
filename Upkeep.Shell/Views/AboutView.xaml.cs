b59051031a30a55443e8a76410de7f39e07ba7e0
Yuri M <yuri.canada@live.com>
2016-05-19 16:36:23 -0700

    Updates

---
+++ Upkeep.Shell/Views/AboutView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Upkeep.Shell.ViewModels;
+
+namespace Upkeep.Shell.Views
+{
+    /// <summary>
+    /// Interaction logic for AboutView.xaml
+    /// </summary>
+    public partial class AboutView : UserControl
+    {
+        public AboutView()
+        {
+            InitializeComponent();
+
+            AboutViewModel model = new AboutViewModel();
+            this.DataContext = model;
+
+        }
+    }
+}
