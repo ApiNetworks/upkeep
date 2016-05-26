a9576c4ef362788e7347cd828aabaae47bddd541
Yuri M <yuri.canada@live.com>
2016-05-26 10:12:17 -0700

    Cleanup

---
+++ Upkeep.Modules.DatabaseExplorer/Models/SqlConnectionInfo.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upkeep.Modules.DatabaseExplorer.Models
+{
+    public class SqlConnectionInfo 
+    {
+        public string Name { get; set; }
+        public string Server { get; set; }
+        public string DatabaseName { get; set; }
+        public string ConnectionString { get; set; }
+        public string ProviderName { get; set; }
+        public string DatabaseLocation { get; set; }
+
+        private string _defaultBackupLocation;
+
+        public string DefaultBackupLocation
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(_defaultBackupLocation) && !String.IsNullOrEmpty(DatabaseLocation))
+                    return DatabaseLocation.ToLower().Replace(".mdf", ".bak");
+                else
+                    return _defaultBackupLocation;
+            }
+            set
+            {
+                _defaultBackupLocation = value;
+            }
+        }
+    }
+}
